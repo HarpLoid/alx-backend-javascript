efdc6f793d9de7d25f8eac64b2d9847d6512e58f
HarpLoid <walesii19@gmail.com>
2024-03-06 22:57:10 +0100

    fixed eslint errors

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,7 +1,7 @@
 export default function appendToEachArrayValue(array, appendString) {
   for (const element of array) {
-	  const i = array.indexOf(element);
-	  array[i] = appendString + element;
+    const i = array.indexOf(element);
+    array[i] = appendString + element;
   }
 
   return array;
--- 0x00-ES6_basic/12-createReportObject.js
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -1,15 +1,15 @@
 export default function createReportObject(employeesList) {
-	const report = {
-		allEmployees: {
-			...employeesList
-		},
-		getNumberOfDepartments() {
-			let count = 0;
-			for (const element in employeesList) {
-				count += 1;
-			}
-			return count;
-		},
-	};
-	return report;
+  const report = {
+    allEmployees: {
+      ...employeesList,
+    },
+    getNumberOfDepartments() {
+      let count = 0;
+      for (const element in employeesList) {
+        count += 1;
+      }
+      return count;
+    },
+  };
+  return report;
 }
