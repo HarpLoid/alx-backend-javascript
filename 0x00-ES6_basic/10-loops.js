24927020d40f2160eca9f77c294632d2ecaca937
HarpLoid <walesii19@gmail.com>
2024-03-06 17:03:15 +0100

    created Objects, for loops, create Objects and created reportObject

---
+++ 0x00-ES6_basic/10-loops.js
@@ -0,0 +1,8 @@
+export default function appendToEachArrayValue(array, appendString) {
+  for (const element of array) {
+	  const i = array.indexOf(element);
+	  array[i] = appendString + element;
+  }
+
+  return array;
+}
