d74b4b60f8d5589e080750247ef036e874f9f8a3
HarpLoid <walesii19@gmail.com>
2024-03-12 09:15:55 +0100

    style: fixed eslint Task 0

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,12 +1,7 @@
-#!/usr/bin/node
-
-export default function getListStudents () {
-
-    const arrayOfObjs = [];
-
-    arrayOfObjs.push({ 'id': 1, 'firstName': 'Guillaume', 'location': 'San Francisco' });
-    arrayOfObjs.push({ 'id': 2, 'firstName': 'James', 'location': 'Columbia' });
-    arrayOfObjs.push({ 'id': 5, 'firstName': 'Serena', 'location': 'San Francisco' });
-
-    return arrayOfObjs;
+export default function getListStudents() {
+  return [
+    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+    { id: 2, firstName: 'James', location: 'Columbia' },
+    { id: 5, firstName: 'Serena', location: 'San Francisco' },
+  ];
 }
